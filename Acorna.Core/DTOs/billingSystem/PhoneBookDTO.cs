ef0cd835dfafbc6054d788c247dbd40239093a42
ahmad.NetDeveloper@Gmail.com <ahmad.NetDeveloper@Gmail.com>
2021-04-30 11:57:51 +0300

    ai-deletedOldProperites

--- Acorna.Core/DTOs/billingSystem/PhoneBookDTO.cs
+++ Acorna.Core/DTOs/billingSystem/PhoneBookDTO.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Acorna.Core.DTOs.billingSystem
+﻿namespace Acorna.Core.DTOs.billingSystem
 {
     public class PhoneBookDTO : BaseDTO
     {
-        //public string PhoneNumber { get; set; }
-        //public string PhoneName { get; set; }
-        //public int PersonalUserId { get; set; }
-        //public string UserName { get; set; }
-        //public string? TypePhoneNumber { get; set; }
-        //public string? statusNumberName { get; set; }
-
         public string PhoneNumber { get; set; }
         public string PhoneName { get; set; }
         public int PersonalUserId { get; set; }
