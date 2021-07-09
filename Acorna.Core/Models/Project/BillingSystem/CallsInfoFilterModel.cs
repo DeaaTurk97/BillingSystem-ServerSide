b51334004e511c15a776498c79a92f6af1794e64
h.hananda@outlook.com <h.hananda@outlook.com>
2021-07-09 16:45:03 +0300

    Fix GetCallSummary bugs

--- Acorna.Core/Models/Project/BillingSystem/CallsInfoFilterModel.cs
+++ Acorna.Core/Models/Project/BillingSystem/CallsInfoFilterModel.cs
@@ -6,8 +6,8 @@ namespace Acorna.Core.Models.Project.BillingSystem.Report
 {
     public class CallsInfoFilterModel
     {
-        public DateTime? FromDate { get; set; }
-        public DateTime? ToDate { get; set; }
+        public string FromDate { get; set; }
+        public string ToDate { get; set; }
         public int? UserId { get; set; }
         public int? GroupId { get; set; }
         public int? ServiceTypeId { get; set; }
